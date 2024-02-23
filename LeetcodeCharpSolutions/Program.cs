cd05f38543ca4b57d94143b72c8adc54dbaa00d0
UlgenHan <alidikmeseg@gmail.com>
2024-02-23 05:16:34 +0300

    Window Tech

--- LeetcodeCharpSolutions/Program.cs
+++ LeetcodeCharpSolutions/Program.cs
@@ -1,14 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using LeetcodeCharpSolutions.Array.Problem001;
+using LeetcodeCharpSolutions.String.Problem003;
 
-Console.WriteLine("Hello, World!");
-int[] numberstest = new int[] { 2, 7, 11, 15 };
-int target = 9;
-
-Problem001 problem001 = new Problem001();
-
-var item = problem001.TwoSumTwoPointerTech(numberstest, target);
-foreach (int i in item)
-{
-    Console.WriteLine(i);   
-}
+string s = "abcabcbb";
+var result = Problem003.LengthOfLongestSubstring(s);
---
+++ LeetcodeCharpSolutions/String/Problem003/Problem003.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeCharpSolutions.String.Problem003
+{
+    public class Problem003
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s == null || s.Length == 0) return 0;
+
+            HashSet<char> characterMap = new HashSet<char>();
+            int left = 0;
+            int maxLength = 0;
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                char currentCharacter = s[right];
+                while (characterMap.Contains(currentCharacter))
+                {
+                    characterMap.Remove(s[left]);
+                    left++;
+                }
+                characterMap.Add(currentCharacter);
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+            return maxLength;
+
+        }
+    }
+}
