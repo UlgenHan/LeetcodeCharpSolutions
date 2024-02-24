e5092c115d2f25d202651b2e4fe02a7a2ccbe2f3
UlgenHan <alidikmeseg@gmail.com>
2024-02-24 23:09:23 +0300

    Problem076 Leetcode Solution

---
+++ LeetcodeCharpSolutions/String/Problem076/Problem076.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeCharpSolutions.String.Problem076
+{
+    public class Problem076
+    {
+        public string MinWindow(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t))
+            {
+                return "";
+            }
+
+            int[] targetCount = new int[128];
+
+            foreach (char c in t)
+            {
+                targetCount[c]++;
+            }
+
+            int left = 0, right = 0;
+            int minLen = int.MaxValue;
+            int minStart = 0;
+            int requiredChars = t.Length;
+
+            while (right < s.Length)
+            {
+                if (targetCount[s[right++]]-- > 0)
+                {
+                    requiredChars--;
+                }
+
+                while (requiredChars == 0)
+                {
+                    if (right - left < minLen)
+                    {
+                        minLen = right - left;
+                        minStart = left;
+                    }
+
+                    if (targetCount[s[left++]]++ == 0)
+                    {
+                        requiredChars++;
+                    }
+                }
+            }
+
+            return (minLen == int.MaxValue) ? "" : s.Substring(minStart, minLen);
+        }
+    }
+}
